9c826d88dbb12ce51ac72f8269f66513973f1201
Nicolas Blaisdell <nblaisdell2@gmail.com>
2023-12-25 10:31:33 -0500

    email sender api up-and-running 3

--- src/app.test.ts
+++ src/app.test.ts
@@ -2,7 +2,7 @@ import request = require("supertest");
 import app from "../src/app";
 
 describe("The Main API", () => {
-  it("should return a status 200 & a Message", () => {
+  it("should return a status 200 & a regular Message", () => {
     return request(app).get("/getEmailFolders").expect(200);
   });
 });
