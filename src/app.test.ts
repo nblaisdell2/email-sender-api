55e8d5c591db1ab08f6af6089c50f118fc787d67
Nicolas Blaisdell <nblaisdell2@gmail.com>
2023-12-25 10:13:22 -0500

    email sender api up-and-running 2

--- src/app.test.ts
+++ src/app.test.ts
@@ -3,19 +3,6 @@ import app from "../src/app";
 
 describe("The Main API", () => {
   it("should return a status 200 & a Message", () => {
-    return request(app)
-      .get("/")
-      .expect(200)
-      .expect("Content-Type", /json/)
-      .then((response) => {
-        console.log("body", response.body);
-        expect(response.body).toEqual(
-          expect.objectContaining({
-            customMessage: expect.objectContaining({
-              msg: "Hello",
-            }),
-          })
-        );
-      });
+    return request(app).get("/getEmailFolders").expect(200);
   });
 });
