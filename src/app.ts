c7d235741f4c6711cd0aa7b3d8ce21715f8a00ba
Nicolas Blaisdell <nblaisdell2@gmail.com>
2023-12-31 07:41:34 -0500

    test 1

--- src/app.ts
+++ src/app.ts
@@ -47,7 +47,7 @@ app.use(async function (
     );
     return res
       .status(data?.status || 200)
-      .end(Buffer.from(data.data.data, "base64"));
+      .end(Buffer.from(data.data.data, "base64").toString("base64"));
   } else {
     return res
       .status(data?.status || 200)
